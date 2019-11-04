4a3707df7f63355d0344df14ed4b9d6c24ceab6d
Bailee Beckles <bbeckles@spartaglobal.com>
2019-11-04 09:16:15 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Read Me 
+
+This is my README for the Json examples 
---
+++ src/main/java/com/spartaglobal/JsonExamples/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.JsonExamples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
